9f16b2386ee1e6ae2ea2280cfed64412c54bc463
ZonePG <zonepg666@gmail.com>
2022-04-25 01:52:43 +0800

    su20-lab01 finished

--- su20-lab-starter/lab01/ll_cycle.c
+++ su20-lab-starter/lab01/ll_cycle.c
@@ -3,5 +3,26 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    if (head == NULL) {
+        return 0;
+    }
+
+    node *tortoise = head;
+    node *hare = head;
+
+    do {
+        hare = hare->next;
+        if (hare == NULL) {
+            return 0;
+        }
+        hare = hare->next;
+
+        tortoise = tortoise->next;
+
+        if (hare == tortoise) {
+            return 1;
+        }
+    } while (tortoise && hare);
+    
     return 0;
 }
