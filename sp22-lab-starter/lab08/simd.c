3c6b4b31fec457a47f36dbe58847263efdf66d67
ZonePG <zonepg666@gmail.com>
2022-05-25 21:25:57 +0800

    sp22-lab08 simd instruction finished

--- sp22-lab-starter/lab08/simd.c
+++ sp22-lab-starter/lab08/simd.c
@@ -53,8 +53,22 @@ long long int sum_simd(int vals[NUM_ELEMS]) {
 
     for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
         /* YOUR CODE GOES HERE */
-
+        __m128i sum_vec = _mm_setzero_si128();
+        for(unsigned int i = 0; i < NUM_ELEMS / 4 * 4; i += 4) {
+            __m128i tmp = _mm_loadu_si128((__m128i *) &vals[i]);
+            __m128i cmp = _mm_cmpgt_epi32(tmp, _127);
+            tmp = _mm_and_si128(tmp, cmp);
+            sum_vec = _mm_add_epi32(sum_vec, tmp);
+        }
+        int tmp_arr[4];
+        _mm_storeu_si128((__m128i *) tmp_arr, sum_vec);
+        result += tmp_arr[0] + tmp_arr[1] + tmp_arr[2] + tmp_arr[3];
         /* Hint: you'll need a tail case. */
+        for(unsigned int i = NUM_ELEMS / 4 * 4; i < NUM_ELEMS; i++) {
+            if (vals[i] >= 128) {
+                result += vals[i];
+            }
+        }
     }
 
     /* DO NOT MODIFY ANYTHING BELOW THIS LINE (in this function) */
@@ -72,8 +86,34 @@ long long int sum_simd_unrolled(int vals[NUM_ELEMS]) {
     for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
         /* YOUR CODE GOES HERE */
         /* Copy your sum_simd() implementation here, and unroll it */
-
+        __m128i sum_vec = _mm_setzero_si128();
+        for(unsigned int i = 0; i < NUM_ELEMS / 16 * 16; i += 16) {
+            __m128i tmp1 = _mm_loadu_si128((__m128i *) &vals[i]);
+            __m128i tmp2 = _mm_loadu_si128((__m128i *) &vals[i + 4]);
+            __m128i tmp3 = _mm_loadu_si128((__m128i *) &vals[i + 8]);
+            __m128i tmp4 = _mm_loadu_si128((__m128i *) &vals[i + 12]);
+            __m128i cmp1 = _mm_cmpgt_epi32(tmp1, _127);
+            __m128i cmp2 = _mm_cmpgt_epi32(tmp2, _127);
+            __m128i cmp3 = _mm_cmpgt_epi32(tmp3, _127);
+            __m128i cmp4 = _mm_cmpgt_epi32(tmp4, _127);
+            tmp1 = _mm_and_si128(tmp1, cmp1);
+            tmp2 = _mm_and_si128(tmp2, cmp2);
+            tmp3 = _mm_and_si128(tmp3, cmp3);
+            tmp4 = _mm_and_si128(tmp4, cmp4);
+            sum_vec = _mm_add_epi32(sum_vec, tmp1);
+            sum_vec = _mm_add_epi32(sum_vec, tmp2);
+            sum_vec = _mm_add_epi32(sum_vec, tmp3);
+            sum_vec = _mm_add_epi32(sum_vec, tmp4);
+        }
+        int tmp_arr[4];
+        _mm_storeu_si128((__m128i *) tmp_arr, sum_vec);
+        result += tmp_arr[0] + tmp_arr[1] + tmp_arr[2] + tmp_arr[3];
         /* Hint: you'll need 1 or maybe 2 tail cases here. */
+        for(unsigned int i = NUM_ELEMS / 16 * 16; i < NUM_ELEMS; i++) {
+            if (vals[i] >= 128) {
+                result += vals[i];
+            }
+        }
     }
 
     /* DO NOT MODIFY ANYTHING BELOW THIS LINE (in this function) */
